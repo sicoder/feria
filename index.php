bed57fc50c9ca0d342592eb42bf3292d98b3c3a8
juaniblade <juaniblade.6@gmail.com>
2014-07-08 09:15:38 -0300

    index by juano
    solamente tiene la estructura con el diseño pedorro

--- index.php
+++ index.php
@@ -1,20 +1,57 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html lang="es">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>FERIA DE CIENCIAS - PICHILA</title>
-<link href="style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Feria de Ciencias</title>
+<link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
+<link href="estilo.css" rel="stylesheet" type="text/css">
+<!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.-->
+<script>var __adobewebfontsappname__="dreamweaver"</script>
+<script src="http://use.edgefonts.net/actor:n4:default.js" type="text/javascript"></script>
+
 </head>
 
 <body>
-<div id="contenedor">
+<div id="espacio">
+	<header>dasd
+asdasdas
+asdsadsa
+asdasds</header>
+	<nav id="menu">
+		<ul>
+			<li><a href="index.php">NOTICIAS</a></li>
+			<li><a href="feria.php">FERIA</a></li>
+			<li><a href="club.php">CLUB</a></li>
+			<li><a href="semanaciencia.php">SEMANA DE LA CIENCIA</a></li>
+			<li><a href="olimpiadas.php">OLIMPIADAS</a></li>
+			<li><a href="galeria.php">GALERÍA</a></li>
+		</ul>
+	</nav>
+	<div id="contenido">
+	<section>
+		<article>
+			<h2>Feria de Ciencias</h2>
+			<p>La feria de ciencias es sadlkasndklsadasdsk y melisa de eguia es puta.</p>
+		</article>
+		<article>
+			<h2>Clubes de Ciencias</h2>
+			<p>Ls clubes de ciencias es sadlkasndklsadasdsk y melisa de eguia es puta.</p>
+		</article>
+	</section>
+	<aside>
+       <h3>Noticias</h3>
+           <p>Aca va una noticia. Pichila.</p>
+       <h3>Noticias</h3>
+           <p>Aca va una noticia. Pichila.</p>
+       <h3>Noticias</h3>
+           <p>Aca va una noticia. Pichila.</p>
+       <h3>Noticias</h3>
+           <p>Aca va una noticia. Pichila.</p>
+    </aside>
+	</div>
+<footer>
+		<div id="pie">Copyright© 2013 - Madrid Juan Ignacio, Tato Cristian. Todos los derechos reservados.</div>
+	</footer>	
 
-<div id="header">FERIA DE CIENCIAS </div>
-<div id="menu">aca va un menu </div>
-<div id="content">aca van las noticias </div>
-<div id="footer">aca van los logos y el copyright el juano </div>
-
-</div>
 </body>
-
-</html>
+</html> 
